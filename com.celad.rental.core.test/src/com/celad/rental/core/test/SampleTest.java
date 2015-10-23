afd26ec6275f1efdb61dcdf99c7898e9e2aec017
localadmin <localadmin@localadmin-VirtualBox>
2015-10-23 17:24:41 +0200

    Tests

---
+++ com.celad.rental.core.test/src/com/celad/rental/core/test/SampleTest.java
@@ -0,0 +1,24 @@
+package com.celad.rental.core.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+
+	  @Test
+	  public void multiplicationOfZeroIntegersShouldReturnZero() {
+
+	    // MyClass is tested
+	    Object tester = new Object();
+
+	    // assert statements
+	    assertEquals("10 x 0 must be 0", 0, 0);
+	    assertEquals("0 x 10 must be 0", 0, 0);
+	    assertEquals("0 x 0 must be 0", 0, 0);
+	  }
+
+	} 
+
+
